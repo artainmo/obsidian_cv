8fddb5bc7b02e7d71df5c2d601909477950ed67a
Arthur Tainmont <53705599+artainmo@users.noreply.github.com>
2025-05-05 19:14:06 +0200

    Update transform.sh

--- obsidian-html/transform.sh
+++ obsidian-html/transform.sh
@@ -1,9 +1,9 @@
 # Use 'sed - i' to make specific changes in 'index.md' if necessary.
 
 # Allow the display of pdf
-sed -i '' 's/<img src="AWS%20Certified%20Cloud%20Practitioner%20certificate.pdf" alt="">/<embed src="AWS%20Certified%20Cloud%20Practitioner%20certificate.pdf" type="application\/pdf" width="100%" height="600px">/g' /output/md/index.md
-sed -i '' 's/<img src="AWS%20Certified%20Cloud%20Practitioner.pdf" alt="">/<embed src="AWS%20Certified%20Cloud%20Practitioner.pdf" type="application\/pdf" width="100%" height="600px">/g' index.md
-sed -i '' 's/<img src="AWS%20Certified%20Machine%20Learning%20-%20Specialty%20certificate.pdf" alt="">/<embed src="AWS%20Certified%20Machine%20Learning%20-%20Specialty%20certificate.pdf" type="application\/pdf" width="100%" height="600px">/g' /output/md/index.md
-sed -i '' 's/<img src="AWS%20Certified%20Machine%20Learning%20-%20Specialty.pdf" alt="">/<embed src="AWS%20Certified%20Machine%20Learning%20-%20Specialty.pdf" type="application\/pdf" width="100%" height="600px">/g' /output/md/index.md
-sed -i '' 's/<img src="Coursera%204V4FNOHS1GBM.pdf" alt="">/<embed src="Coursera%204V4FNOHS1GBM.pdf" type="application\/pdf" width="100%" height="600px">/g' /output/md/index.md
-sed -i '' 's/<img src="Certificate_NSL_Recognised.pdf" alt="">/<embed src="Certificate_NSL_Recognised.pdf" type="application\/pdf" width="100%" height="600px">/g' /output/md/index.md
+sed -i '' 's/![[AWS Certified Cloud Practitioner certificate.pdf]]/<embed src="AWS%20Certified%20Cloud%20Practitioner%20certificate.pdf" type="application\/pdf" width="100%" height="600px">/g' index.md
+sed -i '' 's/![[AWS Certified Cloud Practitioner.pdf]]/<embed src="AWS%20Certified%20Cloud%20Practitioner.pdf" type="application\/pdf" width="100%" height="600px">/g' index.md
+sed -i '' 's/![[AWS Certified Machine Learning - Specialty certificate.pdf]]/<embed src="AWS%20Certified%20Machine%20Learning%20-%20Specialty%20certificate.pdf" type="application\/pdf" width="100%" height="600px">/g' index.md
+sed -i '' 's/![[AWS Certified Machine Learning - Specialty.pdf]]/<embed src="AWS%20Certified%20Machine%20Learning%20-%20Specialty.pdf" type="application\/pdf" width="100%" height="600px">/g' index.md
+sed -i '' 's/![[Coursera 4V4FNOHS1GBM.pdf]]/<embed src="Coursera%204V4FNOHS1GBM.pdf" type="application\/pdf" width="100%" height="600px">/g' index.md
+sed -i '' 's/![[Certificate_NSL_Recognised.pdf]]/<embed src="Certificate_NSL_Recognised.pdf" type="application\/pdf" width="100%" height="600px">/g' index.md
